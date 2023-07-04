04eecef2804b795836d5945b49e40aa84b6a3f42
Kirill Petrikov <119692206+kiryasay@users.noreply.github.com>
2023-07-04 23:15:27 +0300

    АХАХАХАХ

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.onload = () => {
+/* window.onload = () => {
 	const transition_el = document.querySelector('.transition');
 	const anchors = document.querySelectorAll('a');
 
@@ -22,4 +22,16 @@ window.onload = () => {
 			}, 500);
 		});
 	}
-};
+}; */
+VANTA.NET({
+	el: "#intro",
+	mouseControls: true,
+	touchControls: true,
+	gyroControls: false,
+	minHeight: 200.00,
+	minWidth: 200.00,
+	scale: 1.00,
+	scaleMobile: 1.00,
+	color: 0x500505,
+	backgroundColor: 0x1e
+  })
