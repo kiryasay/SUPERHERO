03948b614be457705d02077d797f314b17091810
Bondike <bondike983@gmail.com>
2023-07-08 21:02:23 +0300

    initiall

--- js/find.js
+++ js/find.js
@@ -6,16 +6,16 @@ let searchList = document.getElementById('search-list');
 let activeTab = 1, allData;
 
 const init = () => {
-    showActiveTabBody();
-    showActiveTabHead();
-}
+	showActiveTabBody();
+	showActiveTabHead();
+};
 
 const showActiveTabHead = () => allTabsHead[activeTab - 1].classList.add('active-tab');
 
 const showActiveTabBody = () => {
-    hideAllTabBody();
-    allTabsBody[activeTab - 1].classList.add('show-tab');
-}
+	hideAllTabBody();
+	allTabsBody[activeTab - 1].classList.add('show-tab');
+};
 
 const hideAllTabBody = () => allTabsBody.forEach(singleTabBody => singleTabBody.classList.remove('show-tab'));
 const hideAllTabHead = () => allTabsHead.forEach(singleTabHead => singleTabHead.classList.remove('active-tab'));
@@ -24,194 +24,194 @@ const hideAllTabHead = () => allTabsHead.forEach(singleTabHead => singleTabHead.
 window.addEventListener('DOMContentLoaded', () => init());
 // button event listeners
 allTabsHead.forEach(singleTabHead => {
-    singleTabHead.addEventListener('click', () => {
-        hideAllTabHead();
-        activeTab = singleTabHead.dataset.id;
-        showActiveTabHead();
-        showActiveTabBody();
-    });
+	singleTabHead.addEventListener('click', () => {
+		hideAllTabHead();
+		activeTab = singleTabHead.dataset.id;
+		showActiveTabHead();
+		showActiveTabBody();
+	});
 });
 
 const getInputValue = (event) => {
-    event.preventDefault();
-    let searchText = searchForm.search.value;
-    fetchAllSuperHero(searchText);
-}
+	event.preventDefault();
+	let searchText = searchForm.search.value;
+	fetchAllSuperHero(searchText);
+};
 
 // search form submission
 searchForm.addEventListener('submit', getInputValue);
 
 // api key => 727054372039115
-const fetchAllSuperHero = async(searchText) => {
-    let url = `https://www.superheroapi.com/api.php/106808315798405/search/${searchText}`;
-    try{
-        const response = await fetch(url);
-        allData = await response.json();
-        if(allData.response === 'success'){
-            // console.log(allData);
-            showSearchList(allData.results);
-        }
-    } catch(error){
-        console.log(error);
-    }
-}
+const fetchAllSuperHero = async (searchText) => {
+	let url = `https://www.superheroapi.com/api.php/106808315798405/search/${searchText}`;
+	try {
+		const response = await fetch(url);
+		allData = await response.json();
+		if (allData.response === 'success') {
+			// console.log(allData);
+			showSearchList(allData.results);
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 const showSearchList = (data) => {
-    searchList.innerHTML = "";
-    data.forEach(dataItem => {
-        const divElem = document.createElement('div');
-        divElem.classList.add('search-list-item');
-        divElem.innerHTML = `
-            <img src = "${dataItem.image.url ? dataItem.image.url : ""}" alt = "">
-            <p data-id = "${dataItem.id}">${dataItem.name}</p>
-        `;
-        searchList.appendChild(divElem);
-    });
-}
+	searchList.innerHTML = '';
+	data.forEach(dataItem => {
+		const divElem = document.createElement('div');
+		divElem.classList.add('search-list-item');
+		divElem.innerHTML = `
+			<img src = "${dataItem.image.url ? dataItem.image.url : ''}" alt = "">
+			<p data-id = "${dataItem.id}">${dataItem.name}</p>
+		`;
+		searchList.appendChild(divElem);
+	});
+};
 
 searchForm.search.addEventListener('keyup', () => {
-    if(searchForm.search.value.length > 1){
-        fetchAllSuperHero(searchForm.search.value);
-    } else {
-        searchList.innerHTML = "";
-    }
+	if (searchForm.search.value.length > 1) {
+		fetchAllSuperHero(searchForm.search.value);
+	} else {
+		searchList.innerHTML = '';
+	}
 });
 
 searchList.addEventListener('click', (event) => {
-    let searchId = event.target.dataset.id;
-    let singleData = allData.results.filter(singleData => {
-        return searchId === singleData.id;
-    })
-    showSuperheroDetails(singleData);
-    searchList.innerHTML = "";
+	let searchId = event.target.dataset.id;
+	let singleData = allData.results.filter(singleData => {
+		return searchId === singleData.id;
+	});
+	showSuperheroDetails(singleData);
+	searchList.innerHTML = '';
 });
 
 const showSuperheroDetails = (data) => {
-    console.log(data);
-    document.querySelector('.app-body-content-thumbnail').innerHTML = `
-        <img src = "${data[0].image.url}">
-    `;
+	console.log(data);
+	document.querySelector('.app-body-content-thumbnail').innerHTML = `
+		<img src = "${data[0].image.url}">
+	`;
 
-    document.querySelector('.name').textContent = data[0].name;
-    document.querySelector('.powerstats').innerHTML = `
-    <li>
-        <div>
-            <i class = "fa-solid fa-brain"></i>
-            <span>Интеллект</span>
-        </div>
-        <span>${data[0].powerstats.intelligence}</span>
-    </li>
-    <li>
-        <div>
-            <i class="fa-solid fa-dumbbell"></i>
-            <span>Сила</span>
-        </div>
-        <span>${data[0].powerstats.strength}</span>
-    </li>
-    <li>
-        <div>
-            <i class = "fa-solid fa-person-running"></i>
-            <span>Скорость</span>
-        </div>
-        <span>${data[0].powerstats.speed}</span>
-    </li>
-    <li>
-        <div>
-            <i class = "fa-solid fa-shield"></i>
-            <span>Стойкость</span>
-        </div>
-        <span>${data[0].powerstats.durability}</span>
-    </li>
-    <li>
-        <div>
-            <i class = "fa-solid fa-fire"></i>
-            <span>Энергия</span>
-        </div>
-        <span>${data[0].powerstats.power}</span>
-    </li>
-    <li>
-        <div>
-            <i class = "fa-solid fa-hand-fist"></i>
-            <span>Сражение</span>
-        </div>
-        <span>${data[0].powerstats.combat}</span>
-    </li>
-    `;
+	document.querySelector('.name').textContent = data[0].name;
+	document.querySelector('.powerstats').innerHTML = `
+	<li>
+		<div>
+			<i class = "fa-solid fa-brain"></i>
+			<span>Интеллект</span>
+		</div>
+		<span>${data[0].powerstats.intelligence}</span>
+	</li>
+	<li>
+		<div>
+			<i class="fa-solid fa-dumbbell"></i>
+			<span>Сила</span>
+		</div>
+		<span>${data[0].powerstats.strength}</span>
+	</li>
+	<li>
+		<div>
+			<i class = "fa-solid fa-person-running"></i>
+			<span>Скорость</span>
+		</div>
+		<span>${data[0].powerstats.speed}</span>
+	</li>
+	<li>
+		<div>
+			<i class = "fa-solid fa-shield"></i>
+			<span>Стойкость</span>
+		</div>
+		<span>${data[0].powerstats.durability}</span>
+	</li>
+	<li>
+		<div>
+			<i class = "fa-solid fa-fire"></i>
+			<span>Энергия</span>
+		</div>
+		<span>${data[0].powerstats.power}</span>
+	</li>
+	<li>
+		<div>
+			<i class = "fa-solid fa-hand-fist"></i>
+			<span>Сражение</span>
+		</div>
+		<span>${data[0].powerstats.combat}</span>
+	</li>
+	`;
 
-    document.querySelector('.biography').innerHTML = `
-    <li>
-        <span>Полное имя</span>
-        <span>${data[0].biography['full-name']}</span>
-    </li>
-    <li>
-        <span>Альтер-эго</span>
-        <span>${data[0].biography['alter-egos']}</span>
-    </li>
-    <li>
-        <span>Псевдонимы</span>
-        <span>${data[0].biography['aliases']}</span>
-    </li>
-    <li>
-        <span>Место рождения</span>
-        <span>${data[0].biography['place-of-birth']}</span>
-    </li>
-    <li>
-        <span>Первое появление</span>
-        <span>${data[0].biography['first-appearance']}</span>
-    </li>
-    <li>
-        <span>Издатель</span>
-        <span>${data[0].biography['publisher']}</span>
-    </li>
-    `;
+	document.querySelector('.biography').innerHTML = `
+	<li>
+		<span>Полное имя</span>
+		<span>${data[0].biography['full-name']}</span>
+	</li>
+	<li>
+		<span>Альтер-эго</span>
+		<span>${data[0].biography['alter-egos']}</span>
+	</li>
+	<li>
+		<span>Псевдонимы</span>
+		<span>${data[0].biography['aliases']}</span>
+	</li>
+	<li>
+		<span>Место рождения</span>
+		<span>${data[0].biography['place-of-birth']}</span>
+	</li>
+	<li>
+		<span>Первое появление</span>
+		<span>${data[0].biography['first-appearance']}</span>
+	</li>
+	<li>
+		<span>Издатель</span>
+		<span>${data[0].biography['publisher']}</span>
+	</li>
+	`;
 
-    document.querySelector('.appearance').innerHTML = `
-    <li>
-        <span>
-            <i class = "fa-solid fa-venus-mars"></i> Пол
-        </span>
-        <span>${data[0].appearance['gender']}</span>
-    </li>
-    <li>
-        <span>
-            <i class = "fa-solid fa-person"></i> Раса
-        </span>
-        <span>${data[0].appearance['race']}</span>
-    </li>
-    <li>
-        <span>
-            <i class = "fa-solid fa-arrows-up-down"></i> Рост
-        </span>
-        <span>${data[0].appearance['height'][1]}</span>
-    </li>
-    <li>
-        <span>
-            <i class = "fa-solid fa-weight-hanging"></i> Вес
-        </span>
-        <span>${data[0].appearance['weight'][1]}</span>
-    </li>
-    <li>
-        <span>
-            <i class = "fa-solid fa-eye"></i> Цвет глаз
-        </span>
-        <span>${data[0].appearance['eye-color']}</span>
-    </li>
-    <li>
-        <span>
-            <i class = "fa-solid fa-droplet"></i> Цвет волос
-        </span>
-        <span>${data[0].appearance['hair-color']}</span>
-    </li>
-    `;
+	document.querySelector('.appearance').innerHTML = `
+	<li>
+		<span>
+			<i class = "fa-solid fa-venus-mars"></i> Пол
+		</span>
+		<span>${data[0].appearance['gender']}</span>
+	</li>
+	<li>
+		<span>
+			<i class = "fa-solid fa-person"></i> Раса
+		</span>
+		<span>${data[0].appearance['race']}</span>
+	</li>
+	<li>
+		<span>
+			<i class = "fa-solid fa-arrows-up-down"></i> Рост
+		</span>
+		<span>${data[0].appearance['height'][1]}</span>
+	</li>
+	<li>
+		<span>
+			<i class = "fa-solid fa-weight-hanging"></i> Вес
+		</span>
+		<span>${data[0].appearance['weight'][1]}</span>
+	</li>
+	<li>
+		<span>
+			<i class = "fa-solid fa-eye"></i> Цвет глаз
+		</span>
+		<span>${data[0].appearance['eye-color']}</span>
+	</li>
+	<li>
+		<span>
+			<i class = "fa-solid fa-droplet"></i> Цвет волос
+		</span>
+		<span>${data[0].appearance['hair-color']}</span>
+	</li>
+	`;
 
-    document.querySelector('.connections').innerHTML = `
-    <li>
-        <span>Группы</span>
-        <span>${data[0].connections['group-affiliation']}</span>
-    </li>
-    <li>
-        <span>Родственники</span>
-        <span>${data[0].connections['relatives']}</span>
-    </li>
-    `;
-}
+	document.querySelector('.connections').innerHTML = `
+	<li>
+		<span>Группы</span>
+		<span>${data[0].connections['group-affiliation']}</span>
+	</li>
+	<li>
+		<span>Родственники</span>
+		<span>${data[0].connections['relatives']}</span>
+	</li>
+	`;
+};
