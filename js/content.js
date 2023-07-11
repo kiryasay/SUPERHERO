ad97c9e3e2c6475a0be72b02b7695ea103f431d6
Kirill Petrikov <119692206+kiryasay@users.noreply.github.com>
2023-07-11 18:40:37 +0300

    qwdqwqd

--- js/content.js
+++ js/content.js
@@ -2,7 +2,7 @@ const API_KEY = "58800a88-49e6-4bce-acef-dd16c8f63c4e";
 const API_URL_POPULAR =
   "https://kinopoiskapiunofficial.tech/api/v2.2/films?genres=6&order=RATING&type=ALL&ratingFrom=5&ratingTo=10&yearFrom=1000&yearTo=3000&page=5";
 const API_URL_SEARCH =
-  "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films?order=RATING&type=ALL&ratingFrom=0&ratingTo=10&yearFrom=1000&yearTo=3000&keyword=";
 const API_URL_MOVIE_DETAILS = "https://kinopoiskapiunofficial.tech/api/v2.2/films/"
 
 getMovies(API_URL_POPULAR);
@@ -15,7 +15,7 @@ async function getMovies(url) {
     },
   });
   const respData = await resp.json();
-  showMovies(respData);
+    showMovies(respData);
 }
 
 function getClassByRate(vote) {
@@ -23,6 +23,8 @@ function getClassByRate(vote) {
     return "green";
   } else if (vote > 5) {
     return "orange";
+  } else if(vote == "null"){
+    return "white";
   } else {
     return "red";
   }
@@ -57,12 +59,49 @@ function showMovies(data) {
           `
         <div class="movie__average movie__average--${getClassByRate(
           movie.ratingKinopoisk
-        )}">${movie.ratingKinopoisk}</div>
+        )}">${(movie.ratingKinopoisk == "null")?0: movie.ratingKinopoisk}</div>
         `
         }
       </div>
         `;
     movieEl.addEventListener("click", () => openModal(movie.kinopoiskId))
+    moviesEl.appendChild(movieEl);
+  });
+}
+function showMoviesBySearch(data) {
+  const moviesEl = document.querySelector(".movies");
+
+  // Очищаем предыдущие фильмы
+  document.querySelector(".movies").innerHTML = "";
+
+  data.films.forEach((movie) => {
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+        <div class="movie__cover-inner">
+        <img
+          src="${movie.posterUrlPreview}"
+          class="movie__cover"
+          alt="${movie.nameRu}"
+        />
+        <div class="movie__cover--darkened"></div>
+      </div>
+      <div class="movie__info">
+        <div class="movie__title">${(movie.nameRu != null)?movie.nameRu: movie.nameEn}</div>
+        <div class="movie__category">${movie.genres.map(
+          (genre) => ` ${genre.genre}`
+        )}</div>
+        ${
+          movie.rating &&
+          `
+        <div class="movie__average movie__average--${getClassByRate(
+          movie.rating
+        )}">${(movie.rating == "null")?0: movie.rating}</div>
+        `
+        }
+      </div>
+        `;
+    movieEl.addEventListener("click", () => openModal(movie.filmId))
     moviesEl.appendChild(movieEl);
   });
 }
@@ -108,7 +147,7 @@ async function openModal(id) {
         <li class="modal__movie-genre">Жанр - ${respData.genres.map((el) => `<span>${el.genre}</span>`)}</li>
         ${respData.filmLength ? `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>` : ''}
         <li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
-        <li class="modal__movie-overview">Описание - ${respData.description}</li>
+        <li class="modal__movie-overview">Описание - ${(respData.description == null)?"Отсутствует":respData.description}</li>
       </ul>
       <button type="button" class="modal__button-close">Закрыть</button>
     </div>
