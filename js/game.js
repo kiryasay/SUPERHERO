8968408a4b15bcc612ca6f1f8f6869fb285a2827
Kirill Petrikov <119692206+kiryasay@users.noreply.github.com>
2023-07-13 16:35:45 +0300

    w21qrewrewqreqwrqewrqewrqw

--- js/game.js
+++ js/game.js
@@ -1,9 +1,13 @@
 
+
 const hero = document.getElementById("hero");
 const build = document.getElementById("build");
+const node = document.getElementById("game");
 
 document.addEventListener("keydown", function(event){
-    jump();
+    if ((event.code == 'Space' ) || (event.code == "ArrowUp")){
+        jump();
+      }
 });
 
 function jump() {
@@ -16,11 +20,28 @@ function jump() {
     
 }
 
+
+
+function createBuild(){
+    let object = document.createElement('div');
+    object.setAttribute("class", "build", "id", "build");
+    object.style.backgroundImage = "url('/img/sign.png')";
+    object.style.height = "75px";
+    object.style.width = "30px";
+    object.style.backgroundSize = "30px 75px";
+    object.style.position = "relative";
+    object.style.top = "75px";
+    object.style.animation = "buildMove 1.5s infinite linear";
+    node.appendChild(object);
+    node.removeChild(object);
+}
+setInterval(createBuild, 1000);
+
 let isAlive = setInterval(function(){
     let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
     let buildleft = parseInt(window.getComputedStyle(build).getPropertyValue("left"));
-    if(buildleft < 20 && buildleft > 0 && heroTop >= 430)
+    if(buildleft < 18 && buildleft > 0 && heroTop >= 260 )
     {
-        alert("GameOver");
+        alert("wefewfwe");
     }
 }, 10)
